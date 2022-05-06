a110a571e9c4a5307cecd6038a698df30eb9650e
Lukasz Serich <fargos990@gmail.com>
2022-05-06 14:36:12 +0200

    Przykladowy proces

--- src/app/przyklad/przyklad.component.ts
+++ src/app/przyklad/przyklad.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-przyklad',
   templateUrl: './przyklad.component.html',
   styleUrls: ['./przyklad.component.scss']
 })
-export class PrzykladComponent implements OnInit {
+export class PrzykladComponent {
+  cele = ['Wysyłanie zdjęć, filmów bez zapisywania ich',
+    'Możliwość obróbki ich poprzez przycinanie, nakładanie filtrów, dodanie tekstu itp.',
+    'Możliwość stworzenia własnego "avataru"',
+    'Możliwość zrobienia tzw. "story", czyli zdjęcia, filmu, który usunie się po 24h od opublikowania',
+    'Łączenie aplikacji z bazą danych do zapisywania kont użytkowników.',
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
-
+];
 }
